a6e14db3579ccd091889d3fc0b01a4badec33d75
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2022-11-19 11:39:40 +0300

    find the difference leetcode test

---
+++ find_the_difference/findthedifference.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function (s, t) {
+  function sortString(str) {
+    return str.split("").sort();
+  }
+
+  var str1 = sortString(s);
+  var str2 = sortString(t);
+
+  var longestStrArr = str1.length > str2.length ? str1 : str2;
+
+  for (var i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      return longestStrArr[i];
+    }
+  }
+
+  return longestStrArr[longestStrArr.length - 1];
+};
